8ef13a433ec6664fc1231747f6b940270206fe04
Adam Charane <adamcharane@gmail.com>
2018-12-17 01:17:16 +0100

    Pearson Correlation Coefficient

--- ch2-CollaborativeFiltering/recommender.py
+++ ch2-CollaborativeFiltering/recommender.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
          "Bill":{"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
          "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
@@ -49,3 +51,23 @@ def recommend(username, users):
             recommendations.append((artist, neighborRatings[artist]))
 
     return sorted(recommendations, key= lambda artistTuple: artistTuple[1], reverse=True)
+
+def pearson(user1, user2):
+    sx, sy, sxx, syy, sxy = 0, 0, 0, 0, 0
+    n = 0
+    for key in user1:
+        if key in user2:
+            n += 1
+            x = user1[key]
+            y = user2[key]
+            sx += x
+            sy += y
+            sxx += x**2
+            syy += y**2
+            sxy += x*y
+
+    if n == 0:
+        return 0
+    r = sxy - (sx*sy)/n
+    r /= sqrt(sxx - (sx**2)/n) * sqrt(syy - (sy**2)/n)
+    return r
